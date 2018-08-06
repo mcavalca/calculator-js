41c57192d9d7f44eb4bd8a0ccd23be578d15d316
Marcelo Cavalca Filho <marcelo.cavalca@cptec.inpe.br>
2018-08-06 15:28:45 -0300

    adding buttons (need to practice html css)

--- script.js
+++ script.js
@@ -1,24 +1,35 @@
 /*eslint-env browser*/
 
 var handlers = {
-    sumNumbers: function(){
+    getNumbers: function(){
+        var numOne = parseInt(document.getElementById('firstElement').value);
+        var numTwo = parseInt(document.getElementById('secondElement').value);
         document.getElementById('firstElement').value = '';
         document.getElementById('secondElement').value = '';
+        return [numOne, numTwo];
     },
     doSum: function(){
-        var numOne = parseInt(document.getElementById('firstElement').value);
-        var numTwo = parseInt(document.getElementById('secondElement').value);
-        document.getElementById('firstElement').value = '';
-        document.getElementById('secondElement').value = '';
-        var result = numOne + numTwo;
+        var calc = this.getNumbers();
+        var result = calc[0] + calc[1];
+        if (isNaN(result)) result = 0;
         document.getElementById('output').innerHTML = result;
     },
-    doMin: function(){
-        var numOne = parseInt(document.getElementById('firstElement').value);
-        var numTwo = parseInt(document.getElementById('secondElement').value);
-        document.getElementById('firstElement').value = '';
-        document.getElementById('secondElement').value = '';
-        var result = numOne - numTwo;
-        document.getElementById('output').innerHTML = result;           
+    doMinus: function(){
+        var calc = this.getNumbers();
+        var result = calc[0] - calc[1];
+        if (isNaN(result)) result = 0;
+        document.getElementById('output').innerHTML = result;     
+    },
+    doTimes: function(){
+        var calc = this.getNumbers();
+        var result = calc[0] * calc[1];
+        if (isNaN(result)) result = 0;
+        document.getElementById('output').innerHTML = result;
+    },
+    doDivide: function(){
+        var calc = this.getNumbers();
+        var result = calc[0] / calc[1];
+        if (isNaN(result)) result = 0;
+        document.getElementById('output').innerHTML = result;
     }
 }
